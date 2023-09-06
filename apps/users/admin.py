086643e3535f5a24d1d89ed2af7a375170450b35
sandro <iashvilisandro7@gmail.com>
2023-09-06 11:56:51 +0400

    make password field read_only field

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from apps.users.models import User
 
-# Register your models here.
 
-admin.site.register(User)
+# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    readonly_fields = ('password',)
+
+
+admin.site.register(User, UserAdmin)
